3d8be11a8f7ead53e8b3bfb8bd75d107cd68a225
duanyuanping <1798482104@qq.com>
2020-02-02 20:32:24 +0800

    feat: 页面构建逻辑

--- app/controller/page.ts
+++ app/controller/page.ts
@@ -12,9 +12,9 @@ export default class extends Controller {
 
   public async construct() {
     const { ctx } = this;
-    const { components, page } = ctx.request.body;
+    const { components, page, env } = ctx.request.body;
 
-    const result = await ctx.service.page.construct(page, components);
+    const result = await ctx.service.page.construct(env, page, components);
 
     ctx.body = result;
   }
--- app/service/page.ts
+++ app/service/page.ts
@@ -37,16 +37,14 @@ export default class PageService extends Service {
         msg: '新建页面失败',
       };
     } else {
-      return {
-        code: 0,
-        msg: 'success',
-      };
+      const result = await this.service.page.construct('development', name, []);
+      return result;
     }
   }
 
-  public async construct(page: string, components: ConstructParams[]) {
+  public async construct(env: string, page: string, components: ConstructParams[]) {
     const pageFilePath = path.resolve(pagesFolderPath, page);
-    const names = components.map((item: ConstructParams) => item.name);
+    const names = [ ...new Set(components.map((item: ConstructParams) => item.name)) ];
 
     let npm = 'npm';
     if (shell.which('cnpm')) {
@@ -63,7 +61,8 @@ export default class PageService extends Service {
     }
 
     // 调用组件
-    const demoTempFilePath = path.resolve(pageFilePath, './src/demo.temp.js');
+    const tempFileName = env === 'development' ? 'demo.temp.dev.js' : 'demo.temp.pro.js';
+    const demoTempFilePath = path.resolve(pageFilePath, `./src/${tempFileName}`);
     const demoFilePath = path.resolve(pageFilePath, './src/demo.js');
     let content = fs.readFileSync(demoTempFilePath, 'utf-8');
     // import组件
@@ -74,11 +73,16 @@ export default class PageService extends Service {
     content = content.replace('/** replaceholder: import */', componentImport);
     // 组件调用
     const componentUse = components.map((item: ConstructParams) => {
-      const importName = changeName(item.name);
-      const propString = Object.entries(item.props).map(([ key, val ]) => `${key}={${JSON.stringify(val)}}`).join(' ');
-      return `<${importName} ${propString}/>`;
+      if (env === 'development') {
+        const importName = changeName(item.name);
+        return `{componentClass: ${importName},\ncomponentName: '${item.name}',\nprops: ${JSON.stringify(item.props)},\nkey: Math.floor(Math.random() * 100000)},`;
+      } else {
+        const importName = changeName(item.name);
+        const propString = Object.entries(item.props).map(([ key, val ]) => `${key}={${JSON.stringify(val)}}`).join(' ');
+        return `<div className="component-wrapper"><${importName} ${propString}/></div>`;
+      }
     }).join('\n');
-    content = content.replace('/** replaceholder: use */', componentUse);
+    content = content.replace(`{/** replaceholder: use ${env === 'development' ? 'development' : 'production'} */}`, componentUse);
     fs.writeFileSync(demoFilePath, content, 'utf-8');
     code = shell.exec(`cd ${pageFilePath} && npm run build`).code;
     if (code !== 0) {
