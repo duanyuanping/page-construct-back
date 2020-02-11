1ff3a39159c6605b1e4484e0f2e7b0237d4a53e7
duanyuanping <1798482104@qq.com>
2020-02-11 21:01:01 +0800

    feat: 数据库用户

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,6 +1,25 @@
 import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
-  const config: PowerPartial<EggAppConfig> = {};
+  const config: PowerPartial<EggAppConfig> = {
+    mysql: {
+      client: {
+        // host
+        host: '129.226.186.136',
+        // 端口号
+        port: '3306',
+        // 用户名
+        user: 'root',
+        // 密码
+        password: 'passWord111.',
+        // 数据库名
+        database: 'construct',
+      },
+      // 是否加载到 app 上，默认开启
+      app: true,
+      // 是否加载到 agent 上，默认关闭
+      agent: false,
+    },
+  };
   return config;
 };
